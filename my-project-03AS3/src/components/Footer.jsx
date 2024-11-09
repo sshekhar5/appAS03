e06387dd5f5880b8abf06b8c67ec73dd3c6f45de
shashank <shshekhar684@gmail.com>
2024-11-09 22:38:47 +0530

    font icons added..!!

--- my-project-03AS3/src/components/Footer.jsx
+++ my-project-03AS3/src/components/Footer.jsx
@@ -1,32 +1,39 @@
 import React, { forwardRef } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faYoutube, faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = forwardRef((props, ref) => {
   return (
-    <footer ref={ref} className="bg-red-500 p-6 text-white text-center space-y-4">
+    <footer
+      ref={ref}
+      className="bg-red-500 p-6 text-white text-center space-y-4"
+    >
       <h2 className="text-lg font-bold">AMRIT JANU</h2>
       <p>©2023 All rights Reserved.</p>
       <div className="flex justify-center space-x-4">
-        <a
-          href="https://youtube.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src="https://via.placeholder.com/20" alt="YouTube" />
-        </a>
-        <a
-          href="https://facebook.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src="https://via.placeholder.com/20" alt="Facebook" />
-        </a>
-        <a
-          href="https://instagram.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src="https://via.placeholder.com/20" alt="Instagram" />
-        </a>
+      <a
+        href="https://youtube.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faYoutube} style={{ fontSize: 30, margin: 10 }} />
+      </a>
+
+      <a
+        href="https://facebook.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faFacebook} style={{ fontSize: 30, margin: 10 }} />
+      </a>
+
+      <a
+        href="https://instagram.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faInstagram} style={{ fontSize: 30, margin: 10 }} />
+      </a>
       </div>
     </footer>
   );
